cd6384586c3d50b2ba236159149de0fb0079e814
Max Guglielmi <max.guglielmi@nearmap.com>
2014-09-07 20:07:28 +1000

    Added example HTML file and started changing js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,23 @@ module.exports = function (grunt) {
     grunt.initConfig({
 
         pkg: grunt.file.readJSON('package.json'),
-        
+
         version: 'v1.0',
         dist_folder: 'dist/',
         source_folder: 'src/',
 
         // A list of files, which will be syntax-checked by JSHint
         jshint: {
-        	src: ['Gruntfile.js', /*'<%= source_folder %>tablefilter_all.js'*/],
+            src: ['Gruntfile.js'/*, '<%= source_folder %>tablefilter_all.js'*/],
             options: {
-                '-W069': true, 			// ['xxx'] is better written in dot notation
-                '-W099': true,			// Mixed spaces and tabs
-                '-W004': true,			// 'i' is already defined
-                '-W014': true,			// Bad line breaking before '&&'
-                '-W083': true,			// Don't make functions within a loop
-                '-W086': true,			// Expected a 'break' statement before 'default'
-                '-W049': true,			// Unexpected escaped character '<' in regular expression
-                '-W100': true,		// This character may get silently deleted by one or more browsers
+                // '-W069': true,           // ['xxx'] is better written in dot notation
+                // '-W099': true,           // Mixed spaces and tabs
+                // '-W004': true,           // 'i' is already defined
+                // '-W014': true,           // Bad line breaking before '&&'
+                // '-W083': true,           // Don't make functions within a loop
+                // '-W086': true,           // Expected a 'break' statement before 'default'
+                // '-W049': true,           // Unexpected escaped character '<' in regular expression
+                // '-W100': true,       // This character may get silently deleted by one or more browsers
                 '-W041': true
             }
         },
@@ -44,7 +44,7 @@ module.exports = function (grunt) {
 
             options: {
                 banner: '/*------------------------------------------------------------------------ \n' +
-                        '\t- TableFilter <%= version %> by Max Guglielmi \n' + 
+                        '\t- TableFilter <%= version %> by Max Guglielmi \n' +
                         '\t- build date: <%= grunt.template.today() %> \n' +
                         '\t- http://tablefilter.free.fr \n' +
                         '\t- Copyright (c) 2014, Licensed under the MIT License \n' +
@@ -71,18 +71,18 @@ module.exports = function (grunt) {
                 }
             }
         },
-        
+
         copy: {
-			main: {
-				files: [
-				            { src: ['<%= source_folder %>tablefilter_all.js'], dest: '<%= dist_folder %>tablefilter_all-uncompressed.js' },
+            main: {
+                files: [
+                    { src: ['<%= source_folder %>tablefilter_all.js'], dest: '<%= dist_folder %>tablefilter_all-uncompressed.js' },
                     { src: ['<%= source_folder %>tablefilter.js'], dest: '<%= dist_folder %>tablefilter-uncompressed.js' },
                     { src: ['<%= source_folder %>filtergrid.css'], dest: '<%= dist_folder %>filtergrid-uncompressed.css' },
                     { src: ['**'], cwd: '<%= source_folder %>TF_Modules/', dest: '<%= dist_folder %>TF_Modules/', expand: true },
                     { src: ['**'], cwd: '<%= source_folder %>TF_Themes/', dest: '<%= dist_folder %>TF_Themes/', expand: true }
-				]
-			}
-		}
+                ]
+            }
+        }
     });
 
     // Load the plugins that provide the tasks we specified in package.json.
