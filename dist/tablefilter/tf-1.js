0f5811cdd94ddcdb0e2ff1e367032e135aacd244
Max Guglielmi <max.guglielmi@nearmap.com>
2015-12-11 18:25:12 +1100

    Refined no results container

--- dist/tablefilter/tf-1.js
+++ dist/tablefilter/tf-1.js
@@ -1,6 +1,6 @@
 /** 
- *	 tablefilter v0.0.15 by Max Guglielmi
- *	 build date: 2015-12-10T07:13:22.946Z 
+ *	 tablefilter v0.0.16 by Max Guglielmi
+ *	 build date: 2015-12-11T07:18:51.092Z 
  *	 MIT License  
  */ 
 
