9dcf63866bea3e122f4b23d57d5906d2045a2935
Max Guglielmi <max.guglielmi@nearmap.com>
2015-12-25 13:06:54 +1100

    Run build

--- test/test-filtering.js
+++ test/test-filtering.js
@@ -1,5 +1,8 @@
 var tf = new TableFilter('demo', {
-    base_path: '../dist/tablefilter/'
+    base_path: '../dist/tablefilter/',
+     col_date_type: [
+        null, null, 'ddmmmyyyy'
+    ]
 });
 tf.init();
 
@@ -10,7 +13,7 @@ test('TableFilter object', function() {
     deepEqual(tf.filtersRowIndex, 0, 'Filters row index');
 });
 
-module('Behaviours for issue 74');
+module('Issue 74');
 test('can filter values in this format: 21.1.18 as string', function(){
     tf.setFilterValue(0, '2');
     tf.filter();
@@ -28,6 +31,22 @@ test('can filter values in this format: 21.1.18 as date with operator',
     tf.setFilterValue(1, '>21.1.04');
     tf.filter();
     deepEqual(tf.getValidRows().length, 1, 'Expected number of matches');
+});
+
+module('Issue 72');
+test('can filter values in this format: 21-Jul-2010 as date',
+    function(){
+    tf.clearFilters();
+    tf.setFilterValue(2, '14-Mar-2009');
+    tf.filter();
+    deepEqual(tf.getValidRows().length, 1, 'Expected number of matches');
+});
+test('can filter values in this format: 21-Jul-2010 as date with operator',
+    function(){
+    tf.clearFilters();
+    tf.setFilterValue(2, '<21-Jul-2010');
+    tf.filter();
+    deepEqual(tf.getValidRows().length, 2, 'Expected number of matches');
 });
 
 module('Tear-down');
