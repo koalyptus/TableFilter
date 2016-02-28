b07b390206abe1ecdf6c854bfd6bea4f5b55f33d
koalyptus <undefined>
2016-02-28 09:39:31 +0000

    publish Docs to gh-pages (auto)
    branch:       master
    SHA:          a4d31faa3ae665dd0f8d097f50ad2b04200419f1
    range SHA:    ea53940ccaa1...a4d31faa3ae6
    build id:     112374031
    build number: 169

--- docs/script/pretty-print.js
+++ docs/script/pretty-print.js
@@ -5,8 +5,21 @@
     lines[i].id = 'lineNumber' + (i + 1);
   }
 
+  var matched = location.hash.match(/errorLines=([\d,]+)/);
+  if (matched) {
+    var lines = matched[1].split(',');
+    for (var i = 0; i < lines.length; i++) {
+      var id = '#lineNumber' + lines[i];
+      var el = document.querySelector(id);
+      el.classList.add('error-line');
+    }
+    return;
+  }
+
   if (location.hash) {
-    var line = document.querySelector(location.hash);
+    // ``[ ] . ' " @`` are not valid in DOM id. so must escape these.
+    var id = location.hash.replace(/([\[\].'"@$])/g, '\\$1');
+    var line = document.querySelector(id);
     if (line) line.classList.add('active');
   }
 })();
