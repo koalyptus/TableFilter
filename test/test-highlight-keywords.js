cda05accb5d46cfa0aa9b4c1e02e488df9cf963d
Max Guglielmi <max.guglielmi@nearmap.com>
2016-03-23 19:02:48 +1100

    Initial commit

--- test/test-highlight-keywords.js
+++ test/test-highlight-keywords.js
@@ -9,25 +9,20 @@ var highlightKeyword = tf.feature('highlightKeyword');
 module('Sanity checks');
 test('HighlightKeyword component', function() {
     deepEqual(typeof highlightKeyword, 'object', 'Instanciated');
-    deepEqual(highlightKeyword.highlightedNodes instanceof Array,
-        true, 'Property check');
+    deepEqual(highlightKeyword.highlightCssClass, 'keyword', 'Css class check');
 });
 
 test('Highlighted keywords', function() {
     tf.setFilterValue(1, 'Perth');
     tf.setFilterValue(3, '3.1');
     tf.filter();
-    deepEqual(highlightKeyword.highlightedNodes.length,
-        2, 'Number of highlighted words');
     deepEqual(tf.tbl.querySelectorAll('.keyword').length, 2,
         'Number of applied CSS classes');
 
     tf.clearFilters();
-    deepEqual(highlightKeyword.highlightedNodes.length,
-        0, 'Number of highlighted words');
-    // TODO: uncomment when fixed
-    // deepEqual(tf.tbl.querySelectorAll('.keyword').length, 0,
-    //     'Number of applied CSS classes');
+    // FIXED with issue 155 :)
+    deepEqual(tf.tbl.querySelectorAll('.keyword').length, 0,
+        'Number of applied CSS classes');
 });
 
 module('Reset feature');
@@ -35,8 +30,7 @@ test('can destroy and init TableFilter', function() {
     tf.destroy();
     tf.init();
     deepEqual(typeof highlightKeyword, 'object', 'Instanciated');
-    deepEqual(highlightKeyword.highlightedNodes instanceof Array,
-        true, 'Property check');
+    deepEqual(highlightKeyword.highlightCssClass, 'keyword', 'Css class check');
 });
 
 module('Tear-down');
@@ -46,7 +40,7 @@ test('can destroy TableFilter DOM elements and clean highlighted words',
         tf.filter();
         tf.destroy();
         deepEqual(tf.hasGrid(), false, 'Filters removed');
-        deepEqual(highlightKeyword.highlightedNodes.length,
-            0, 'Number of highlighted words');
+        deepEqual(tf.tbl.querySelectorAll('.keyword').length, 0,
+            'Number of applied CSS classes');
     }
 );
