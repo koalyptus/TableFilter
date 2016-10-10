288af8b94e04447cb46a6e379fd79e167bdf1d2c
Max Guglielmi <max.guglielmi@nearmap.com>
2016-10-10 18:16:39 +1100

    Addressed issue-309

--- test/test-highlight-keywords.js
+++ test/test-highlight-keywords.js
@@ -12,6 +12,7 @@ test('HighlightKeyword component', function() {
     deepEqual(highlightKeyword.highlightCssClass, 'keyword', 'Css class check');
 });
 
+module('Behaviour');
 test('Highlighted keywords', function() {
     tf.setFilterValue(1, 'Perth');
     tf.setFilterValue(3, '3.1');
@@ -23,6 +24,24 @@ test('Highlighted keywords', function() {
     // issue 155
     deepEqual(tf.tbl.querySelectorAll('.keyword').length, 0,
         'Number of applied CSS classes');
+});
+
+// issue 309
+test('Match same term with increasing number of chars', function() {
+    tf.setFilterValue(1, 'Pe');
+    tf.filter();
+    deepEqual(tf.tbl.querySelectorAll('.keyword').length, 1,
+        'Search term matched');
+
+    tf.setFilterValue(1, 'Per');
+    tf.filter();
+    deepEqual(tf.tbl.querySelectorAll('.keyword').length, 1,
+        'Search term matched');
+
+    tf.setFilterValue(1, 'Pert');
+    tf.filter();
+    deepEqual(tf.tbl.querySelectorAll('.keyword').length, 1,
+        'Search term matched');
 });
 
 module('Reset feature');
