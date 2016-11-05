a72fb11b1945655723bf69b4dbf2c4516b402a66
koalyptus <undefined>
2016-11-05 11:27:57 +0000

    publish TableFilter to gh-pages (auto)
    branch:       master
    SHA:          3e977fa6e51203a0fd64e4920530092f9c03babd
    range SHA:    f4960e250ba9...3e977fa6e512
    build id:     173479329
    build number: 470

--- tablefilter/tf-1.js
+++ tablefilter/tf-1.js
@@ -1,6 +1,6 @@
 /** 
- *	 tablefilter v0.4.7 by Max Guglielmi
- *	 build date: 2016-11-05T11:13:05.474Z 
+ *	 tablefilter v0.4.8 by Max Guglielmi
+ *	 build date: 2016-11-05T11:24:11.198Z 
  *	 MIT License  
  */ 
 
