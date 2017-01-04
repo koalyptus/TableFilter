9d8744392a9fed5bd4e7451e56cef5c32fb77940
Max Guglielmi <max.guglielmi@nearmap.com>
2017-01-04 14:09:18 +1100

    Fix instantiation with 1 row

---
+++ test/test-no-rows.js
@@ -0,0 +1,10 @@
+
+
+module('TableFilter with no rows');
+test('throws when no rows', function() {
+    throws(
+        function() { new TableFilter('demo') },
+        Error,
+        'Throws Error when DOM table does not contain rows'
+    );
+});
