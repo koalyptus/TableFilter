722c420dc694dc00ae43297a09a305f8c3b14b9c
koalyptus <maxgug@hotmail.com>
2017-12-07 22:48:00 +1100

    prepare dist

--- dist/tablefilter/tf-0-789aa3c9851693c1f448.js
+++ dist/tablefilter/tf-0-789aa3c9851693c1f448.js
@@ -1,6 +1,6 @@
 /** 
- *	 tablefilter v0.6.8 by Max Guglielmi
- *	 build date: 2017-12-07T11:19:43.905Z 
+ *	 tablefilter v0.6.7 by Max Guglielmi
+ *	 build date: 2017-12-07T11:38:37.217Z 
  *	 MIT License  
  */ 
 
