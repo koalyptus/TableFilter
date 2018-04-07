025717f61ce3ebd1dd5cb84c63cd1d80c0a2ffdf
koalyptus <maxgug@hotmail.com>
2018-04-07 10:45:33 +1000

    introduce default column width

--- test/test-col-widths.js
+++ test/test-col-widths.js
@@ -13,7 +13,7 @@
         deepEqual(tf instanceof TableFilter, true, 'TableFilter instanciated');
         deepEqual(cols[1].style.width, '100px', 'Expected column width');
         deepEqual(cols[4].style.width, '', 'Expected column width');
-        deepEqual(tf.dom().style.width, '545px', 'Table width set');
+        deepEqual(tf.dom().style.width, '645px', 'Table width set');
         deepEqual(tf.dom().style.tableLayout, 'fixed', 'Table layout fixed');
     });
 
@@ -22,14 +22,14 @@
         tf = null;
         tf = new TableFilter('demo', {
             base_path: '../dist/tablefilter/',
-            col_widths: ['150px', '100px', '175px', '120px', '200px'],
+            col_widths: ['150px', '100px', '175px', '120px', null],
             grid_layout: true
         });
         tf.init();
         var gridLayout = tf.feature('gridLayout');
         var cols = gridLayout.headTbl.getElementsByTagName('col');
         deepEqual(cols[0].style.width, '150px', 'Expected column width');
-        deepEqual(cols[4].style.width, '200px', 'Expected column width');
+        deepEqual(cols[3].style.width, '120px', 'Expected column width');
         deepEqual(
             tf.dom().style.width === gridLayout.headTbl.style.width,
             true,
